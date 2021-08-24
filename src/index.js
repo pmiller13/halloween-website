8bad413c2a216693564ca453c15b7a4e116627fa
p <patrick.mill3r13@gmail.com>
2021-08-23 23:26:54 -0700

    Adding basic event handling for TextField and Button. Also removed unused imports

--- src/index.js
+++ src/index.js
@@ -1,32 +1,39 @@
 import React from 'react';
-import useState from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {your_name} from './const.js';
 import {Button, TextField} from '@material-ui/core';
-import { InputAdornment } from '@material-ui/core';
 
-
-function Foobar() {
-    //var input = document.getElementById('outlined-basic').text;
-    //const [title, setTitle] = useState('');
-
-    return 10;
-}
 
 class FormSubmission extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+         welcome: your_name(),
+         inputtext: "",
+         data: 'many universes in many bubbles'
+      }
+    this.handleEvent = this.handleEvent.bind(this);
+    this.handleTextFieldChange = this.handleTextFieldChange.bind(this);
+  }
+  handleTextFieldChange(i){
+    console.log(i);
+  }
+  handleEvent(){
+    //var this_text = this.state.inputtext;
+    this.setState({inputtext: ''});
+  }
   render() {
-    const msg = your_name();
     return (
       <div>
         <div className="parentparent">
-          <h1 className="parent">Welcome, 💀 {msg}</h1>
+          <h1 className="parent">Welcome, 💀 {this.state.welcome}</h1>
         </div>
         <div className="parentparent">
-          <TextField style = {{width: 350}} id="outlined-basic" ref="Text01" placeholder="many universes in many bubbles"  variant="outlined" className="text-input parent" />
+          <TextField style = {{width: 350}} id="outlined-basic" onChange={swag => this.handleTextFieldChange(swag.target.value)} defaultValue={this.state.inputtext} placeholder={this.state.data} variant="outlined" className="text-input parent" />
         </div>
         <div className="parentparent">
-          <Button className="parent" variant="contained" onClick={() => { console.log(Foobar()); }}>Submit</Button>
+          <Button className="parent" variant="contained" onClick={() => this.handleEvent()}>Submit</Button>
         </div>
       </div>
     )
