d3072ffb6455ccfbcb61823674e739dfae3999e6
p <patrick.mill3r13@gmail.com>
2021-08-29 21:20:29 -0700

    Refactored textbox and button into its own component and pushing submit now sends textbox data to queue using axios

--- src/index.js
+++ src/index.js
@@ -1,47 +1,64 @@
 import React from 'react';
+import {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {your_name} from './const.js';
+import axios from 'axios';
 import {Button, TextField} from '@material-ui/core';
 
 
-class FormSubmission extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-         welcome: your_name(),
-         inputtext: "",
-         placeholdertext: "many universes in many bubbles",
-      }
-    this.handleEvent = this.handleEvent.bind(this);
-    this.handleTextFieldChange = this.handleTextFieldChange.bind(this);
-  }
-  handleTextFieldChange(i){
-    this.setState({inputtext: i});
-    console.log(i);
-  }
-  handleEvent(){
-    //var this_text = this.state.inputtext;
-    this.setState({inputtext: ''});
-  }
-  render() {
+function FormSubmission() {
+    const [input_text, textfieldChange] = useState("");
+    const placeholdertext = "many universes in many bubbles";
+    function handleSubmit() {
+        if (input_text === "") {
+            return 1;
+        }
+        const payload = JSON.stringify(
+            {
+                'text': input_text
+            }
+        );
+        axios.post('http://0.0.0.0:8080/upload', payload,
+            {
+                headers:{'Access-Control-Allow-Origin' : '0.0.0.0:8080'},
+                auth:{username: 'yolo', password: '123swagswag321'}
+            })
+            .then(function (response) {
+                textfieldChange('');
+
+            })
+            .catch(function (error) {
+                console.log(error);
+            }
+        );
+        return 0;
+    }
+    return (
+        <div>
+            <div className="parentparent">
+              <TextField style = {{width: 350}} id="outlined-basic" value={input_text} onChange={event => textfieldChange(event.target.value)} placeholder={placeholdertext} variant="outlined" className="text-input parent" />
+            </div>
+            <div className="parentparent">
+              <Button className="parent" variant="contained" onClick={handleSubmit}>Submit</Button>
+            </div>
+        </div>
+    );
+}
+
+function FuncForm() {
+    const welcome = your_name();
     return (
       <div>
         <div className="parentparent">
-          <h1 className="parent">Welcome, 💀 {this.state.welcome}</h1>
+          <h1 className="parent">Welcome, 💀 {welcome}</h1>
         </div>
-        <div className="parentparent">
-          <TextField style = {{width: 350}} id="outlined-basic" value={this.state.inputtext} onChange={swag => this.handleTextFieldChange(swag.target.value)} placeholder={this.state.placeholdertext} variant="outlined" className="text-input parent" />
-        </div>
-        <div className="parentparent">
-          <Button className="parent" variant="contained" onClick={() => this.handleEvent()}>Submit</Button>
-        </div>
+        <FormSubmission />
       </div>
-    )
-  }
+    );
 }
 
 ReactDOM.render(
-  <FormSubmission />,
+  <FuncForm />,
   document.getElementById('root')
 );
